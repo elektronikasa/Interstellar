d01e386c8742cb570fa36808717a989baaa2e7a4
James <james@sleepwalker.co.za>
2019-06-14 21:39:18 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Interstellar
-Future Frontend - Modern JS Build Toolkit for next level frontend systems.
+# Annotated webpack 4 Config
+
+This is the companion github repo for the [An Annotated webpack 4 Config for Frontend Web Development](https://nystudio107.com/blog/an-annotated-webpack-4-config-for-frontend-web-development) article.
+
+It contains the full webpack config and ancillary files discussed in the article.
+
+Please see the article for details.
---
+++ src/vue/Confetti.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+    </main>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import VueConfetti from 'vue-confetti'
+
+    import Vuesax from 'vuesax'
+
+    import 'vuesax/dist/vuesax.css' //Vuesax styles
+    Vue.use(Vuesax)
+
+    Vue.use(VueConfetti);
+
+    export default {
+        mounted: function() {
+            this.$confetti.start({
+                shape: 'heart',
+                colors: ['DodgerBlue', 'OliveDrab', 'Gold', 'pink', 'SlateBlue', 'lightblue', 'Violet', 'PaleGreen', 'SteelBlue', 'SandyBrown', 'Chocolate', 'Crimson'],
+            });
+            setTimeout(() => {
+                this.$confetti.stop();
+            }, 5000);
+        },
+        methods: {}
+    }
+</script>
